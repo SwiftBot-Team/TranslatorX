681705b795fd775e0211d787f402936974253429
Lucas340 <lucasstorres100@gmail.com>
2020-11-26 19:39:15 -0300

    🧪- Used this file for tests. Later I will put some examples here.

--- examples/index.ts
+++ examples/index.ts
@@ -1,11 +1,13 @@
 import Locale from '../src';
 
 async function a() {
-  let l = new Locale('examples/locales')
+  let l = new Locale('examples/locales', {
+    returnUndefined: true,
+    defaultLanguage: 'pt',
+    debug: true
+  })
 
   await l.init()
-
-  l.t('commands:hello2.hello3')
 
   l.setLang('pt')
 }
